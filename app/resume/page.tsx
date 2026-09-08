db66b114b545887afcb40418dbc6c9e6001bbf11
Sam Boateng <32736261+Boatengs@users.noreply.github.com>
2026-09-08 13:22:33 -0400

    Replace portfolio resume with September 8 version

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { SiteNav } from "../site-nav";
 
-const resumeUrl = "/sampson-boateng-resume.pdf?v=20260907-1";
+const resumeUrl = "/sampson-boateng-resume.pdf?v=20260908-1";
 
 export default function Resume() {
   return (
